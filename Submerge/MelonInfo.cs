5f44f39a127bcf0bd6cd5ddd699cb6b1f33ad0fc
Checkerb0ard <Colehennessy1@gmail.com>
2023-12-17 15:05:55 -0800

    dum fix

--- Submerge/MelonInfo.cs
+++ Submerge/MelonInfo.cs
@@ -1,5 +1,5 @@
 ﻿using MelonLoader;
 using Submerge; // The namespace of your mod class
 // ...
-[assembly: MelonInfo(typeof(Main), "Submerge", "0.0.1", "SilverWare Team")]
+[assembly: MelonInfo(typeof(Mod), "Submerge", "0.0.1", "SilverWare Team")]
 [assembly: MelonGame("Unknown Worlds", "Subnautica")]
