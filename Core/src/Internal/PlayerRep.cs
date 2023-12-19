aa715fca92d4b93178245486ecf38bb53b96a12c
Checkerb0ard <Colehennessy1@gmail.com>
2023-12-18 16:23:12 -0800

    happy happy happy

---
+++ Core/src/Internal/PlayerRep.cs
@@ -0,0 +1,37 @@
+﻿using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace Submerg.Internal
+{
+    public class PlayerRep
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+
+        public GameObject LocalRep = Player.mainObject;
+
+        public PlayerRep(Transform transform)
+        {
+            Position = transform.position;
+            Rotation = transform.rotation;
+        }
+
+        /*public static void CheckForRep() 
+        {
+            if (Player.mainObject != null)
+            {
+                MelonLogger.Msg("Player isnt null");
+            }
+            else
+            {
+                MelonLogger.Msg("Player is null or not found");
+            }
+        }*/
+    }
+}
---
+++ Core/src/Patching/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using static UnityEngine.UI.Selectable;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Submerge.Patching;
+
+[HarmonyPatch(typeof(uGUI_MainMenu))]
+internal class MainMenuPatches
+{
+    [HarmonyPatch(nameof(uGUI_MainMenu.Awake))]
+    [HarmonyPostfix]
+    private static void AwakePatch(uGUI_MainMenu __instance)
+    {
+        var playButton = __instance.gameObject.GetComponentInChildren<MainMenuPrimaryOptionsMenu>().transform.Find("PrimaryOptions/MenuButtons/ButtonPlay").gameObject;
+        var modManagerButton = Object.Instantiate(playButton);
+        modManagerButton.GetComponent<RectTransform>().SetParent(playButton.transform.parent, false);
+        modManagerButton.name = "Multiplayer";
+        var text = modManagerButton.GetComponentInChildren<TextMeshProUGUI>();
+        text.text = "Multiplayer";
+        Object.DestroyImmediate(text.gameObject.GetComponent<TranslationLiveUpdate>());
+        modManagerButton.transform.SetSiblingIndex(1);
+        var button = modManagerButton.GetComponent<Button>();
+        button.onClick = new Button.ButtonClickedEvent();
+        button.onClick.AddListener(null);
+    }
+}
